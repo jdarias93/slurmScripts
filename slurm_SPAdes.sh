2a885d51e32227f2f9c429012d5a67c0429104f4
Joshua Arias <josh.d.arias@gmail.com>
2018-09-19 14:06:46 -0700

    Optimized for fastq.gz files. Objective: CraVe

--- slurm_SPAdes.sh
+++ slurm_SPAdes.sh
@@ -4,17 +4,16 @@
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --time=1-00:00:00    
-#SBATCH --output=NeuAf_spade_slurmout
+#SBATCH --output=CraVe_spade_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="NeuAf_spade"
+#SBATCH --job-name="CraVe_spade"
 #SBATCH -p batch
 
-# Turned off the BayesHammer error correction tool with only-assembler
-# Will try both to see if there's a difference.
+# You MUST have fastq files.
 
 module load SPAdes
-srun spades.py --careful --only-assembler --12 NeuAf_Interlaced.fasta -o spades_output
+srun spades.py --careful -1 CraVe1.fastq.gz -2 CraVe2.fastq.gz -o spades_output
 
 hostname
 date
