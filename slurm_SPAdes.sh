ac43f00cb8cdfc9032aea17ebe9545f142d2bcae
Joshua Arias <josh.d.arias@gmail.com>
2018-09-25 11:18:26 -0700

    SPAdes slurm script that finally worked

--- slurm_SPAdes.sh
+++ slurm_SPAdes.sh
@@ -3,18 +3,17 @@
 #SBATCH --nodes=1    
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=16GB
-#SBATCH --time=1-00:00:00    
+#SBATCH --time=5-00:00:00    
 #SBATCH --output=CraVe_spade_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --job-name="CraVe_spade"
 #SBATCH -p batch
 
-# Updated: 09/20/18
 # You MUST have fastq files.
 
 module load SPAdes
-srun spades.py -k 21,33,55,77,99,127 -1 CraVe1.fastq.gz -2 CraVe2.fastq.gz -o spades_output
+srun spades.py -k 21,33,55,77,99,127 -1 CraVe_P1.fastq.gz -2 CraVe_P2.fastq.gz -o spades_output_092418
 
 hostname
 date
