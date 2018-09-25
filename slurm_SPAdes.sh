07aca6419baa55f55f4dd37cb01c3863c4b4b068
Joshua Arias <josh.d.arias@gmail.com>
2018-09-25 11:45:10 -0700

    Generalized the format.

--- slurm_SPAdes.sh
+++ slurm_SPAdes.sh
@@ -7,13 +7,19 @@
 #SBATCH --output=CraVe_spade_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="CraVe_spade"
+#SBATCH --job-name="<SPECIES>_spade"
 #SBATCH -p batch
 
-# You MUST have fastq files.
+# This is ONLY for SPAdes Version 3.12.0
+# You MUST have fastq.gz files.
+# Used this k-mer distribution because it is recommended by the SPAdes manual
+# Ensure that you are using RAW data, though you might be able to get processed files to work.
 
 module load SPAdes
-srun spades.py -k 21,33,55,77,99,127 -1 CraVe_P1.fastq.gz -2 CraVe_P2.fastq.gz -o spades_output_092418
+srun spades.py -k 21,33,55,77,99,127 -1 <FILE 1>.fastq.gz -2 <FILE 2>.fastq.gz -o spades_output
+
+# This is only for Paired End reads. If you don't have Paired End reads, go here and make the approriate changes to this:
+# http://cab.spbu.ru/files/release3.12.0/manual.html
 
 hostname
 date
