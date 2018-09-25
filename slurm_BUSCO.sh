220ffa5c95b46aa36d60d54fb2ca292dd6e0d894
Joshua Arias <josh.d.arias@gmail.com>
2018-09-25 12:24:05 -0700

    Added BUSCO slurm.

---
+++ slurm_BUSCO.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1    
+#SBATCH --ntasks=16
+#SBATCH --mem-per-cpu=16GB
+#SBATCH --time=5-00:00:00    
+#SBATCH --output=<SPECIES>_BUSCO_slurmout
+#SBATCH --mail-user=josh.d.arias@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="<SPECIES>_BUSCO"
+#SBATCH -p batch
+
+# This is ONLY for BUSCO version 3.0.2.
+
+module load BUSCO/3.0.2
+srun run_BUSCO.py -i <FASTA FILE> -o <SPECIES> -l 
+
+hostname
+date
