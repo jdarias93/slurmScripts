347f40e81c7c7a5c69538dbae62b1b3df6c82303
Joshua Arias <josh.d.arias@gmail.com>
2018-10-01 16:43:04 -0700

    Initial edits to AUGUSTUS slurm

--- slurm_AUGUSTUS.sh
+++ slurm_AUGUSTUS.sh
@@ -2,18 +2,23 @@
 
 #SBATCH --nodes=1    
 #SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=16GB
-#SBATCH --time=5-00:00:00    
-#SBATCH --output=<SPECIES>_BUSCO_slurmout
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --time=10-00:00:00
+#SBATCH --output=<SPECIES>_AUGUSTUS_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="<SPECIES>_BUSCO"
+#SBATCH --job-name="<SPECIES>_AUGUSTUS"
 #SBATCH -p batch
 
-# This is ONLY for BUSCO version 3.0.2.
+# Max DNA piece size picked according to largest contig amount, held by NeuAf at the time of writing this.
+# For help on modifying this, consult these resources:
+# https://sites.google.com/site/yijyunluo/Bioinformatics/Gene-prediction
+# http://avrilomics.blogspot.com/2013/04/training-augustus-gene-finding-software.html
+# http://augustus.gobics.de/binaries/README.TXT
 
-module load BUSCO/3.0.2
-srun run_BUSCO.py -i <FASTA FILE> -o <SPECIES> -l /rhome/jarias/bigdata/BUSCO/embryophyta_odb9.tar.gz -m geno -c 16
+module load augustus
+srun augustus --genemodel=partial --maxDNAPieceSize=840000 --protein=on --species=arabidopsis <queryfilename.fasta>
+srun augustus --genemodel=partial --maxDNAPieceSize=840000 --protein=on --species=tomato <queryfilename.fasta>
 
 hostname
 date
