e1653929fec1b9356baefc49e351c1adfb30208f
Joshua Arias <josh.d.arias@gmail.com>
2018-10-12 14:56:49 -0700

    BUSCO working like a charm now.

--- slurm_BUSCO.sh
+++ slurm_BUSCO.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
-#SBATCH --nodes=1    
+#SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=16GB
-#SBATCH --time=10-00:00:00    
+#SBATCH --time=10-00:00:00
 #SBATCH --output=<SPECIES>_BUSCO_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
@@ -13,7 +13,15 @@
 # This is ONLY for BUSCO version 3.0.2.
 
 module load busco/3.0.2
-srun run_BUSCO.py -i <FASTA FILE> -o <SPECIES> -l /rhome/jarias/bigdata/BUSCO/embryophyta_odb9.tar.gz -m geno -c 16
+
+# You need to set up an AUGUSTUS path that you have permission to edit--the HPCC global AUGUSTUS install will not let you
+# The workaround is to copy the HPCC augustus build to your local path:
+module unload augustus/3.3 # Releases global install hook up
+export PATH="/path/to/BUSCO/augustus3.3/bin:$PATH" # Sets new path variables for your local augustus install
+export PATH="/path/to/bigdata/BUSCO/augustus3.3/scripts:$PATH"
+export AUGUSTUS_CONFIG_PATH="/path/to/bigdata/BUSCO/augustus3.3/config/"
+
+srun run_BUSCO.py -i <SPECIES_PATH> -o <OUTPUT_NAME> -l /path/to/BUSCOdb -m geno -c 16
 
 hostname
 date
