a375bdcd3e46840b31a077c698adc7145c8451fd
Joshua Arias <josh.d.arias@gmail.com>
2018-10-12 13:17:32 -0700

    Fixing BUSCO

--- slurm_BUSCO.sh
+++ slurm_BUSCO.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1    
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=16GB
-#SBATCH --time=5-00:00:00    
+#SBATCH --time=10-00:00:00    
 #SBATCH --output=<SPECIES>_BUSCO_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
@@ -12,7 +12,7 @@
 
 # This is ONLY for BUSCO version 3.0.2.
 
-module load BUSCO/3.0.2
+module load busco/3.0.2
 srun run_BUSCO.py -i <FASTA FILE> -o <SPECIES> -l /rhome/jarias/bigdata/BUSCO/embryophyta_odb9.tar.gz -m geno -c 16
 
 hostname
---
+++ slurm_OF.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --mem-per-cpu=16GB
+#SBATCH --time=10-00:00:00
+#SBATCH --output=<SPECIES>_CDHIT_slurmout
+#SBATCH --mail-user=josh.d.arias@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="<SPECIES>_CDHIT"
+#SBATCH -p batch
+
+module load cd-hit
