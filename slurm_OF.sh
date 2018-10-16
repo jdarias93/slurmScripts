12a1a8b4d13d9bab4d14958ff71ebee29a49ccef
Joshua Arias <josh.d.arias@gmail.com>
2018-10-16 11:56:30 -0700

    Updated OF slurm.

--- slurm_OF.sh
+++ slurm_OF.sh
@@ -4,10 +4,17 @@
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --time=10-00:00:00
-#SBATCH --output=<SPECIES>_CDHIT_slurmout
+#SBATCH --output=OF_slurmout
 #SBATCH --mail-user=josh.d.arias@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="<SPECIES>_CDHIT"
+#SBATCH --job-name="OF_Test"
 #SBATCH -p batch
 
-module load cd-hit
+module load diamond
+## You need to get mmseqs2 and hook it to $PATH. This next line	does it	for you, but you have to provide the correct working directory.
+export PATH=$PATH:`pwd`/mmseqs2/bin/
+module load mcl
+module load fastme
+
+srun /rhome/jarias/bigdata/OrthoFinder-2.2.7/orthofinder -f /rhome/jarias/bigdata/OrthoFinder-2.2.7/<FASTA FILE DIRECTORY> -S diamond -t 16
+date
