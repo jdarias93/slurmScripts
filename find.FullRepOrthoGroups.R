238038657798a5e611ea0a2eb8ab9b2ecf7412e2
Lab Students <labstudents@Amandas-iMac-3.local>
2018-11-24 16:55:23 -0800

    Current Working Version

--- find.FullRepOrthoGroups.R
+++ find.FullRepOrthoGroups.R
@@ -1,5 +1,5 @@
 # Written by Joshua Arias
-# 11/20/2018
+# Updated 11/24/2018
 # Correspondence: josh.d.arias@gmail.com
 
 # find.FullRepOrthoGroups
@@ -13,6 +13,8 @@
 # meaning every column is explicitly delimited by a comma. This isn't immediately
 # done in the OF output file (oddly enough) so you have to do it manually.
 # It is, however a quick fix. Figure it out ;)
+
+# ENSURE that your OF Gene_Tree and Alignment folders are INSIDE YOUR WORKING DIRECTORY!
 
 find.FullRepOrthoGroups <- function(OrthoGroups) {
         a <- OrthoGroups
@@ -40,13 +42,16 @@ find.FullRepOrthoGroups <- function(OrthoGroups) {
         }
         write.csv(x=OG[as.logical(z)], file="FullRepOG.csv") 
         # Write list of full rep OGs
-        c <- read.csv("FullRepOG.csv")
-        c <- c[,2]
-        dir.create(paste(getwd(),"/FullOGs",sep=""))
+        c <- read.csv("FullRepOG.csv")          # Read "FullRepOG.csv"
+        c <- c[,2]                              # Create vector of Full-rep OGs
+        dir.create(paste(getwd(),"/FullOGs",sep=""))    # Create folder to place .fa and trees
         for (i in 1:length(c)) {
-                fromdir <- paste(getwd(),"/Gene_Trees/",c[i], "_tree.txt",sep="")
+                fromdir <- paste(getwd(),"/Gene_Trees/",c[i], "_tree.txt",sep="") 
+                # Directory Creation for Trees
                 todir <- paste(getwd(),"/FullOGs/",c[i],"_tree.txt",sep="")
-                file.rename(from=fromdir, to=todir)
+                # Directory Creation for Trees
+                file.rename(from=fromdir, to=todir)     # Move pertinent files to new directory
+                # Next three commands do the same but for alignments
                 fromdir <- paste(getwd(),"/Alignments/",c[i], ".fa",sep="")
                 todir <- paste(getwd(),"/FullOGs/",c[i],".fa",sep="")
                 file.rename(from=fromdir, to=todir)
